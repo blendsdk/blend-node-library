73b35691c3276d4d58e8abd00290c963adc4a9c6
Gevik Babakhani <g.babakhani@truesoftware.nl>
2016-06-14 22:56:20 +0200

    Added basic Filesystem tests

--- tests/tests/sanity-test.ts
+++ tests/tests/sanity-test.ts
@@ -2,8 +2,3 @@ runner.defineTest("Sanity Test", function (t: Blend.testing.TestRunner) {
     t.assertTrue(true, "Sanity check");
     t.done();
 });
-
-runner.defineTest("Sanity Test", function (t: Blend.testing.TestRunner) {
-    t.assertTrue(false, "Sanity check");
-    t.done();
-});
